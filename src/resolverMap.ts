fe4d47c0ba4c84d3ce599fec3e8e86361bcc990c
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2020-03-13 22:47:37 +0100

    Add simple pagination

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,25 +1,52 @@
 import { IResolvers } from 'graphql-tools'
 import { mockPlaces } from './mockPlaces'
 
-interface IArgs {
-  search: string
+type PlaceType = {
+  id: string
+  host: string
+  beds: number
+  name: string
+  city: string
+  price: string
+  rating: string
+  country: string
+  details: string
+  slides: Array<string>
+  imageUrl: string
 }
 
-const handleSearching = (searchTerm: string) => {
-  const filteredByCity = mockPlaces.filter(place => place.city.toLowerCase().includes(searchTerm))
+interface IArgs {
+  search: string
+  pageSize: number
+  pageIndex: number
+}
+
+const handleSearching = (places: Array<PlaceType>, args: IArgs) => {
+  const searchTerm = args.search
+  const filteredByCity = places.filter(place => place.city.toLowerCase().includes(searchTerm))
   if (filteredByCity.length < 1) {
-    return mockPlaces.filter(place => place.country.toLowerCase().includes(searchTerm))
+    return places.filter(place => place.country.toLowerCase().includes(searchTerm))
   }
   return filteredByCity
+}
+
+const handlePagination = (places: Array<PlaceType>, args: IArgs) => {
+  const pageSize = args.pageSize || 20
+  const pageIndex = args.pageIndex || 1
+  return places.slice((pageIndex - 1) * pageSize, pageIndex * pageSize)
 }
 
 const resolverMap: IResolvers = {
   Query: {
     getPlaces(_: void, args: IArgs): Object {
-      let places = handleSearching(args.search)
+      const allPlaces = mockPlaces
+      const count = allPlaces.length
+      let places = handleSearching(allPlaces, args)
+      places = handlePagination(places, args)
+      
       return {
         places,
-        count: places.length,
+        count,
       };
     },
   },
